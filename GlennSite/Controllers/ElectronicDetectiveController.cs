56f5b860fb1e955c92339233a428051354761c6b
Glenn Seplowitz <fredzachrcool@optonline.net>
2015-10-18 13:13:00 -0400

    Built up the main layout and navigation bar.  Have a layout of the interface.  Entering the instructions on how to play Electronic Detective.

---
+++ GlennSite/Controllers/ElectronicDetectiveController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GlennSite.Controllers
+{
+    public class ElectronicDetectiveController : Controller
+    {
+        // GET: ElectronicDetective
+        public ActionResult ElectronicDetective()
+        {
+            return View();
+        }
+
+        public ActionResult HowToPlay()
+        {
+            return View();
+        }
+    }
+}
