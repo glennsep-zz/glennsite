69f50bce85008d739cb641a52f3ad8dc5f45e26f
Glenn Seplowitz <fredzachrcool@optonline.net>
2016-09-28 20:45:34 -0400

    Created stored procedures to hold Case Fact Sheet and started to code PlayGame class to create murder and store murder facts to Case Fact Sheet tables.

--- GlennSite/Controllers/HomeController.cs
+++ GlennSite/Controllers/HomeController.cs
@@ -12,7 +12,6 @@ namespace GlennSite.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            Murder game = new Murder();
             
             return View();
         }
